ca24efc1ae6c2496c462da73791b71c1dc6166c2
Seoyun0208 <84954568+Seoyun0208@users.noreply.github.com>
2022-04-20 03:19:51 +0900

    [CHORE] drive_away_bugs 내 파일 경로 수정
    github page 에 사용하기 위한  drive away bugs (벌레 쫓아내기) 게임 프로젝트 내 파일 경로 수정

--- main.js
+++ main.js
@@ -27,11 +27,11 @@ const gameBtn = document.querySelector(".game__button");
 const bgRestart = document.querySelector(".restart");
 
 // ! 사운드
-const carrotSound = new Audio("/sound/carrot_pull.mp3");
-const bugSound = new Audio("/sound/bug_pull.mp3");
-const alertSound = new Audio("/sound/alert.wav");
-const bgSound = new Audio("/sound/bg.mp3");
-const gameWinSound = new Audio("/sound/game_win.mp3");
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const gameWinSound = new Audio("./sound/game_win.mp3");
 
 // ! 함수
 // 게임 시작 모달창 -> initGame, startGame
