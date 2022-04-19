47a160cd7cbe3c5b35d559859ed127effdd92d32
Seoyun0208 <84954568+Seoyun0208@users.noreply.github.com>
2022-04-19 22:50:43 +0900

    [FEAT] drive_away_bugs item 전체 기능 구현
    drive away bugs (벌레 쫓아내기) 게임 프로젝트 내 각종 기능 구현
    - 게임 시작, 정지 및 종료
    - 타이머 및 점수기록판
    - 상황에 맞는 모달창

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 "use strict";
-const carrotSize = 80;
+
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
 
+// 게임 초기화
 function initGame() {
-	addItem("carrot", 5, "img/carrot.png");
-	addItem("bug", 5, "img/bug.png");
+	started = false;
+	field.innerHTML = "";
+	gameScore.innerText = 5;
+
+	addItem("bug", BUG_COUNT, "img/bug.png");
+	addItem("carrot", CARROT_COUNT, "img/carrot.png");
 }
 
+// field 에 아이템 랜덤 배치
 function addItem(className, cnt, imgPath) {
 	const x1 = 0;
 	const y1 = 0;
-	const x2 = fieldRect.width - carrotSize;
-	const y2 = fieldRect.height - carrotSize;
+	const x2 = fieldRect.width - CARROT_SIZE;
+	const y2 = fieldRect.height - CARROT_SIZE;
 
 	for (let i = 0; i < cnt; i++) {
 		const item = document.createElement("img");
@@ -31,6 +41,136 @@ function addItem(className, cnt, imgPath) {
 
 function randomNumber(min, max) {
 	return Math.random() * (max - min) + min;
+}
+
+// 게임 시작
+let started = false;
+const GAME_DURATION = 5;
+
+function gameStart() {
+	gameTimerStart();
+	started = true;
+}
+
+// 게임 타이머 시작
+let timer = undefined;
+
+function gameTimerStart() {
+	let time = GAME_DURATION;
+	timer = setInterval(() => {
+		updateGameTImerText(time);
+		--time;
+		if (time < 0) {
+			clearInterval(timer);
+			bgRefresh.classList.add("open");
+			score = 0;
+		}
+	}, 1000);
+}
+
+// 게임 타이머 정지
+function gameTimerStop(timer) {
+	started = false;
+	clearInterval(timer);
+	scoreBoard();
+}
+
+// 브라우저 게임 타이머 텍스트
+const gameTimer = document.querySelector(".game__timer");
+
+function updateGameTImerText(time) {
+	gameTimer.innerText = time;
+}
+
+// 게임 시작 모달창
+const modalStart = document.querySelector(".modal__start");
+const bgPlay = document.querySelector(".play");
+const gameHeader = document.querySelector(".game__header");
+
+modalStart.addEventListener("click", (e) => {
+	if (
+		e.target === e.currentTarget ||
+		e.target.parentNode === e.currentTarget
+	) {
+		bgPlay.classList.remove("open");
+		gameHeader.classList.add("show");
+		initGame();
+		gameStart();
+	}
+});
+
+// 게임 정지 모달창
+const gameBtn = document.querySelector(".game__button");
+const bgRestart = document.querySelector(".restart");
+
+gameBtn.addEventListener("click", () => {
+	bgRestart.classList.add("open");
+	gameTimerStop(timer);
+});
+
+// 게임 다시시작(restart, refresh) 모달창
+const restartBtn = document.querySelector(".restart__button");
+const bgRefresh = document.querySelector(".refresh");
+const refreshBtn = document.querySelector(".refresh__button");
+
+restartBtn.addEventListener("click", () => {
+	bgRestart.classList.remove("open");
+	initGame();
+	gameStart();
+});
+
+refreshBtn.addEventListener("click", () => {
+	bgRefresh.classList.remove("open");
+	initGame();
+	gameStart();
+});
+
+// field 의 아이템 이벤트
+const gameScore = document.querySelector(".game__score");
+const bug = document.querySelector(".bug");
+const msgRefresh = document.querySelector(".modal__message-refresh");
+
+field.addEventListener("click", onFieldClick);
+
+function onFieldClick(e) {
+	const target = e.target;
+	if (target.matches(".bug")) {
+		// 버그
+		target.remove();
+		score++;
+	} else if (target.matches(".carrot")) {
+		// 당근
+		score--;
+		finishGame("lose");
+	}
+
+	scoreBoard();
+
+	if (score === BUG_COUNT) {
+		finishGame("win");
+	}
+}
+
+// 게임 종료
+function finishGame(winOrLose) {
+	gameTimerStop(timer);
+	bgRefresh.classList.add("open");
+	if (winOrLose === "win") {
+		msgRefresh.innerText = "YOU WON";
+	} else if (winOrLose === "lose") {
+		msgRefresh.innerText = "YOU LOST";
+	}
+}
+
+// 점수 기록판
+let score = 0;
+
+function scoreBoard() {
+	if (started) {
+		gameScore.innerText = BUG_COUNT - score;
+	} else {
+		score = 0;
+	}
 }
 
 initGame();
